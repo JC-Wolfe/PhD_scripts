a0004f7f99a4387e73017d166e1696f1dbd3b357
JC-Wolfe <wolfe.jareth@gmail.com>
2022-03-16 17:57:40 +0000

    Tidying

--- Drosophila_Scripts/Plots/Completed_Paper_Plots/Review_Plots/B&W_No_Fs(1)h_AUC.R
+++
@@ -1,9 +0,0 @@
-library(pROC)
-
-Arch <- read.csv("~/Review_Paper_Plots/arch_prot_rules/data/No_fs(1)h_bulk.tsv", sep = "\t")
-
-v1 <- roc(Arch$IsCommon, Arch$Conf..Perc.1)
-
-pdf("~/Review_Paper_Plots/arch_prot_rules/plots/PRROC/No_Fs(1)h_B&W_roc.pdf")
-plot(v1, main = "Common vs Putative ROC")
-dev.off()
--- Drosophila_Scripts/Plots/Completed_Paper_Plots/Supp_Figures/Supp1/Supp_1_Final.R
+++
@@ -1,5 +0,0 @@
-
-library(MLeval)
-
-BG3_XAI <- get(load("/home/jw18713/Project1/data/model_predictions/BG3_FuzzyGR.Rda"))
-S2_XAI <- get(load("/home/jw18713/Project1/data/model_predictions/S2_FuzzyGR.Rda"))
